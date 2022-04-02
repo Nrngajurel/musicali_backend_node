9f75d793b65eaa7e92ddda62b37e3dae90751aa1
shanand <shanand.codehimalaya@gmail.com>
2022-04-02 17:47:09 +0545

    added router and controller for node backend

---
+++ api/users/user.controller.js
@@ -0,0 +1,127 @@
+const {
+    create,
+    getAllUser,
+    getUserById,
+    updateUser,
+    deleteUser,
+    getUserByEmail
+} = require("./user.service");
+
+const { genSaltSync, hashSync, compareSync } = require("bcrypt");
+const { sign } = require("jsonwebtoken")
+
+module.exports = {
+    createUser: (req,res)=>{
+        const body = req.body;
+        const salt = genSaltSync(10);
+        body.password = hashSync(body.password, salt);
+        create(body,(err,results)=>{
+            if(err){
+                console.log(err);
+                return res.status(500).json({
+                    success: 0,
+                    message: "database connection error"
+                });
+            }
+            return res.status(200).json({
+                success: 1,
+                message: results
+            });
+        });
+    },
+    getUserById: (req,res)=>{
+        const id = req.params.id;
+        getUserById(id,(err,results)=>{
+            if(err){
+                console.log(err);
+                return;
+            }
+            if(!results){
+                return res.json({
+                    success: 0,
+                    message: "Record not found"
+                });
+            }
+            return res.json({
+                success: 1,
+                message: results
+            });
+        });
+    },
+    getUsers: (req,res)=>{
+        this.getUsers((err,results)=>{
+            if(err){
+                console.log(err);
+                return;
+            }
+            return res.json({
+                success: 1,
+                message: results
+            });
+        });
+    },
+    updateUser: (req,res)=>{
+        const body = req.body;
+        const salt = genSaltSync(10);
+        body.password = hashSync(body.password, salt);
+        updateUser(body,(err,results)=>{
+            if(err){
+                console.log(err);
+                return;
+            }
+            return res.json({
+                success: 1,
+                message: "updated successfully"
+            });
+        });
+    },
+    deleteUser: (req,res)=>{
+        const data = req.body;
+        deleteUser(data, (err,results)=>{
+            if(err){
+                console.log(err);
+                return;
+            }
+            if(!results){
+                return res.json({
+                    success: 0,
+                    message: "User deleted successfully"
+                });
+            }
+            return res.json({
+                success: 1,
+                message: "User deleted successfully"
+            });
+        });
+    },
+    login: (req,res)=>{
+        const body = req.body;
+        getUserByEmail(body.email, (err,results)=>{
+            if(err){
+                console.log(err);
+            }
+            if(!results){
+                return res.json({
+                    success: 0,
+                    data: "Invalid password or email"
+                });
+            }
+            const result = compareSync(body.password, results.password);
+            if(result){
+                results.password = undefined;
+                const jsonToken = sign({ result: results }, process.env.JWT_KEY, {
+                    expiresIn: "24h"
+                });
+                return res.json({
+                    success: 1,
+                    token: jsonToken
+                });
+            }else{
+                return res.json({
+                    success: 0,
+                    data: "Invalid email or password"
+                });
+            }
+        });
+    }
+}
---
+++ api/users/user.service.js
@@ -0,0 +1,72 @@
+const pool = require("../../config/database");
+
+module.exports = {
+    create: (data, callBack)=>{
+        pool.query(`insert into customer(name, address, number, password, email)
+                    values(?,?,?,?,?)`),
+                    [
+                        data.name,
+                        data.address,
+                        data.number,
+                        data.password,
+                        data.email
+                    ],
+                    (error,results,fields)=>{
+                        if(error){
+                            return callBack(error);
+                        }
+                        return callBack(null,results);
+                    } 
+    },
+    getAllUser: callBack=>{
+        pool.query(`select id, name, email, password, address, number from customer`),
+        [],
+        (error,results,fields)=>{
+            if(error) return callBack(error);
+            return callBack(null, results);
+        }
+    },
+    getUserById: (id,callBack)=>{
+        pool.query(`select name, address, number, email from customer where id = ?`),
+        [id],
+        (error,results,fields)=>{
+            if(error) return callBack(error);
+            return callBack(null,results[0]);
+        }
+    },
+    updateUser: (data,callBack)=>{
+        pool.query(
+            `update customer set name=?, address=?, password=?, email=?, password=? 
+            where id = ?`,
+            [
+            data.name,
+            data.address,
+            data.password,
+            data.email,
+            data.password,
+            data.id
+        ],
+        (error,results,fields)=>{
+            if(error) return callBack(error);
+            return callBack(null, results[0])
+        });
+    },
+    deleteUser: (data,callBack)=>{
+        pool.query(`delete from customer where id=?`,
+        [data.id],
+        (error,results,fileds)=>{
+            if(error) return callBack(error);
+            return callBack(null,results[0]);
+        });
+    },
+    getUserByEmail: (data,callBack)=>{
+        pool.query(`select * from customer where email=?`,
+        [data.email],
+        (error,results,fields)=>{
+            if(error){
+                callBack(error);
+            }
+            return callBack(null,results[0]);
+        })
+    }
+};  
